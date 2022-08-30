4d4fe08393ea66b95f13f3620600834d90e60b46
elbarqy <elbarqy.dev@gmail.com>
2022-08-30 17:39:49 +0200

    fixing the ngrx state mangement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Todo } from './Todo';
 import { Store } from '@ngrx/store';
-import { StoreInterface } from './store/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  //the state of this component
   todos: Todo[] = [];
+  //the new item to be sent to the store
   newTodo: string;
+  //the store observable
+  store$: Observable<Array<Todo>>;
 
-  constructor(private store:Store<StoreInterface>){
-      this.store.subscribe(data => this.newTodo = data.list.name)
+  //inject the store with
+  constructor(private store: Store<{ list: Array<Todo> }>) {
+    //select the state you want to listien on
+    this.store$ = this.store.select('list');
+  }
+  //subscribe to the store and update the state
+  ngOnInit(): void {
+    this.store$.subscribe((items) => {
+      console.log('recieved ', items);
+      this.todos = [...items];
+    });
   }
 
-  saveTodo(){
-    if(this.newTodo){
-      this.store.dispatch({type: 'add', name:this.newTodo})
-      this.newTodo =""
-    }
-    else{
-      alert("Please enter todo")
+  saveTodo() {
+    if (this.newTodo) {
+      this.store.dispatch({ type: 'add', name: this.newTodo });
+      this.newTodo = '';
+    } else {
+      alert('Please enter todo');
     }
   }
 
-
-  done(id:number){
+  done(id: number) {
     this.todos[id].isCompleted = !this.todos[id].isCompleted;
   }
 
-  remove(id:number){
-    this.store.dispatch({type: 'delete' } )
-    this.todos = this.todos.filter((v,i) => i !== id)
+  remove(id: number) {
+    this.store.dispatch({ type: 'delete' });
+    this.todos = this.todos.filter((v, i) => i !== id);
   }
-
 }
--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,33 +1,26 @@
+import { Todo } from './../Todo';
 
+let initialState: Array<Todo> = [];
 
-export interface StoreInterface{
-  list: List
+interface CustomAction {
+  type: string;
+  name: string;
 }
 
-interface List {
-    id: number,
-    name:string
-}
-
-let initialState:any = []
-
-interface CustomAction{
-  type: string,
-  name:string
-}
-
-export function reducer(state = initialState , action: any){
-      switch(action.type){
-        case 'add':
-          let todo ={
-            id: state.length + 1,
-            name: action.name
-          };
-      return initialState.push(todo);
-        case 'delete':
-          return
-        case 'update':
-          return
-        default: return state
-      }
+export function reducer(state = initialState, action: any) {
+  switch (action.type) {
+    case 'add':
+      let todo = {
+        name: action.name,
+        isCompleted: false,
+      };
+      console.log(todo)
+      return [...state, todo];
+    case 'delete':
+      return;
+    case 'update':
+      return;
+    default:
+      return state;
+  }
 }
